622b87497dfbd3f7fa50bb4686144ce91a887a06
Ciwan Ceylan <ciwan.ceylan@outlook.com>
2024-05-16 23:10:56 +0200

    Add env file and kddcup dataset

--- scripts/large_datasets.py
+++ scripts/large_datasets.py
@@ -31,6 +31,69 @@ def load_musae_git(as_dense: bool):
     return X, y
 
 
+def load_kddcup1999():
+    feature_dtypes = {
+        "duration": float,
+        "protocol_type": 'category',
+        "service": 'category',
+        "flag": 'category',
+        "src_bytes": float,
+        "dst_bytes": float,
+        "land": 'category',
+        "wrong_fragment": float,
+        "urgent": float,
+        "hot": float,
+        "num_failed_logins": float,
+        "logged_in": 'category',
+        "num_compromised": float,
+        "root_shell": float,
+        "su_attempted": float,
+        "num_root": float,
+        "num_file_creations": float,
+        "num_shells": float,
+        "num_access_files": float,
+        "num_outbound_cmds": float,
+        "is_host_login": 'category',
+        "is_guest_login": 'category',
+        "count": float,
+        "srv_count": float,
+        "serror_rate": float,
+        "srv_serror_rate": float,
+        "rerror_rate": float,
+        "srv_rerror_rate": float,
+        "same_srv_rate": float,
+        "diff_srv_rate": float,
+        "srv_diff_host_rate": float,
+        "dst_host_count": float,
+        "dst_host_srv_count": float,
+        "dst_host_same_srv_rate": float,
+        "dst_host_diff_srv_rate": float,
+        "dst_host_same_src_port_rate": float,
+        "dst_host_srv_diff_host_rate": float,
+        "dst_host_serror_rate": float,
+        "dst_host_srv_serror_rate": float,
+        "dst_host_rerror_rate": float,
+        "dst_host_srv_rerror_rate": float,
+        "label": 'category'
+    }
+
+    train_data = pd.read_csv(
+        "datasets/kddcup99/kddcup.data",
+        names=list(feature_dtypes.keys()),
+        dtype=feature_dtypes
+    )
+    y = pd.factorize(train_data.pop('label'))
+    del feature_dtypes['label']
+    test_data = pd.read_csv(
+        "datasets/kddcup99/kddcup.testdata.unlabeled",
+        names=list(feature_dtypes.keys()),
+        dtype=feature_dtypes
+    )
+    data = pd.concat((train_data, test_data), axis=0, ignore_index=True)
+    X_data = pd.get_dummies(data).astype(float).to_numpy()
+    return X_data, y
+
+
 def time_fs(X_data, device: Optional[torch.device]):
     start = time.perf_counter()
     pruned_x, recon_errors, feature_norms = qmrfs.qmr_fs(
@@ -41,19 +104,20 @@ def time_fs(X_data, device: Optional[torch.device]):
         device=device
     )
     duration = time.perf_counter() - start
-    num_orig_features = X_data.shape[1]
     num_kept_features = pruned_x.shape[1]
-    return duration, num_orig_features, num_kept_features
+    return duration, num_kept_features
 
 
 def main():
-    X_data, y = load_musae_git(as_dense=True)
-
+    # X_data, y = load_musae_git(as_dense=True)
+    X_data, y = load_kddcup1999()
     # X_data, X_orig, y = load_us_census()
 
-    duration, num_orig_features, num_kept_features = time_fs(X_data, device=torch.device('cpu'))
+    n, m = X_data.shape
+    print("Num. instances: ", n)
+    print("Num. features: ", m)
+    duration, num_kept_features = time_fs(X_data, device=torch.device('cuda'))
     print("Duration: ", duration)
-    print("Num start dim.: ", num_orig_features)
     print("Num end dim.: ", num_kept_features)
 
 
